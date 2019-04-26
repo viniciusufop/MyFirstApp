76dfde7a4ea15efaed8d64343489d59609f82e32
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-04-26 15:17:45 -0300

    criando segunda activity

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myfirstapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,17 @@ class MainActivity : AppCompatActivity() {
         // val myToast = Toast.makeText(this, message, duration);
         val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
         myToast.show()
+    }
+
+    fun countMe (view: View) {
+        // Get the text view
+        val showCountTextView = findViewById<TextView>(R.id.textView)
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+        // Display the new value in the text view.
+        showCountTextView.text = count.toString()
     }
 }
